1ebf9bde2453daf4c694d2ca9d6405b794f2a748
Junbeeom <wns_p@naver.com>
2024-01-07 20:32:55 +0900

    init

---
+++ src/main/java/com/test/force/Java_App_Board_SpringBoot_v8/JavaAppBoardSpringBootV8Application.java
@@ -0,0 +1,13 @@
+package com.test.force.Java_App_Board_SpringBoot_v8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppBoardSpringBootV8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppBoardSpringBootV8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/force/Java_App_Board_SpringBoot_v8/JavaAppBoardSpringBootV8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.force.Java_App_Board_SpringBoot_v8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppBoardSpringBootV8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
